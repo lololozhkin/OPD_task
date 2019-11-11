20438da64e22b3f52f727d369d4ba9ad8cf28247
Nikita <42915946+asdminerpro@users.noreply.github.com>
2019-11-12 01:02:32 +0500

    Create Json.cs

---
+++ challenge-starterkit-master/ConsoleCoreApp/Json.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleCoreApp
+{
+    public static class Json
+    {
+        public static string GetAnswer(string a)
+        {
+            a = a.Replace(" ", ",");
+            a = a.Replace(":{", " ");
+            a = a.Replace(":", ",");
+            a = a.Replace("{", String.Empty);
+            a = a.Replace("}", String.Empty);
+            a = a.Replace("  ", " ");
+            var current = new StringBuilder();
+            var flag = true;
+            int answer = 0;
+            Console.WriteLine(a);
+            foreach (var i in a)
+            {
+                if (!flag && i != ',')
+                {
+                    current.Append(i);
+                }
+                if (i == ',' && !flag)
+                {
+                    answer += int.Parse(current.ToString());
+                    current = new StringBuilder();
+                    flag = true;
+                }
+                else if (i == ',')
+                {
+                    flag = false;
+                }
+            }
+
+            if (!flag)
+            {
+                answer += int.Parse(current.ToString());
+            }
+
+            return answer.ToString();
+        }
+    }
+}
