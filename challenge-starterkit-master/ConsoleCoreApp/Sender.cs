be78cce41c0672c22da1440161811f11a1cc7795
trololozhkin <alexand3r.lozhkin@gmail.com>
2019-11-14 01:03:31 +0500

    edited senders task creation

--- challenge-starterkit-master/ConsoleCoreApp/Sender.cs
+++ challenge-starterkit-master/ConsoleCoreApp/Sender.cs
@@ -6,12 +6,22 @@ namespace ConsoleCoreApp
 {
     public class Sender
     {
+        private string GetNonRelisedMathSolution(string task)
+        {
+            Console.WriteLine(task);
+            Console.WriteLine("Я хуй занет как это решать, пиздец просто, помоги");
+            var ans = Console.ReadLine();
+            Console.WriteLine("Ну хуле, если ты такой уверенны блять");
+            return ans;
+        }
+
         private string GetAns(string type, string task)
         {
             type = GetTypeOfTheTask(type);
             return type switch
             {
-                "math" => MathSolution.GetAns(task),
+                //"math" => MathSolution.GetAns(task),
+                "math" => GetNonRelisedMathSolution(task),
                 "polynomial-root" => PolynomsSolver.GetRoot(task),
                 "shape" => ShapeSolver.GetAnswer(task),
                 "determinant" => MatrixDetSolver.GetAns(task),
@@ -30,15 +40,15 @@ namespace ConsoleCoreApp
             {
                 @"Вычисли значение выражения в целых числах или комплексных числах с целыми действительной и мнимой частями. Примеры ответов через запятую: 4, i, 2i, 4+3i, 4-i"
                 => "math",
-                @"Посчитай определитель матрицы. Во входных данных строки разделяются двойным обратным слэшем \\, а столбцы амперсандом &."
+                @"Посчитай определитель матрицы. Во входных данных строки матрицы разделяются двойным обратным слэшем \\, а столбцы - амперсандом &."
                 => "determinant",
                 @"Найди любой корень полинома, либо передай в качестве ответа ""no roots"", если корней нет." =>
                 "polynomial-root",
-                @"Расшифруй сообщение, используя подсказки. Все сообщения взяты из Ты-Знаешь-Какого-Текста. Но текст был предварительно подготовлен, поэтому сообщения содержат только латинские буквы в нижнем регистре, цифры, апостроф (') и пробелы. Пробелы не повторяются."
+                @"Расшифруй сообщение, используя подсказки. Все сообщения взяты из Ты-Знаешь-Какого-Текста. Но текст был предварительно подготовлен, поэтому сообщения содержат только латинские буквы в нижнем регистре, цифры, апостроф (') и пробелы. Остальные символы заменены на пробелы, а повторы пробелов заменены на одиночные пробелы."
                 => "cypher",
-                @"Определи, что за фигура задана точками, а затем выбери соответствующий ответ из предложенных" =>
+                @"Определи, что за фигура задана точками, а затем выбери соответствующий ответ из предложенных." =>
                 "shape",
-                @"Найди обратную матрицу. Во входыных данных и в ответе строки матрицы разделяются двойным обратынм слэшем \\, а столбцы - амперсандом &. Если матрица необратима, то передай ""unsolvable"" в качестве ответа."
+                @"Найди обратную матрицу. Во входных данных и в ответе строки матрицы разделяются двойным обратным слэшем \\, а столбцы - амперсандом &. Если матрица необратима, то передай ""unsolvable"" в качестве ответа."
                 => "inverse-matrix",
                 @"Верни описанное число в цифровом представлении." => "string-number",
                 @"Посчитай сумму значений JSON-объекта." => "json",
@@ -61,12 +71,12 @@ namespace ConsoleCoreApp
         }
         //*/
 
-        public void SendSomeAnswers(ChallengeClient challengeClient, ChallengeResponse challenge, string type,
+        public void SendSomeAnswers(ChallengeClient challengeClient, ChallengeResponse challenge,
             int count)
         {
             for (var i = 0; i < count; i++)
             {
-                if (!SendAnswer(challengeClient, challenge, type))
+                if (!SendAnswer(challengeClient, challenge))
                 {
                     Console.WriteLine("Wrong answer!1!");
                     break;
@@ -75,17 +85,17 @@ namespace ConsoleCoreApp
             }
         }
 
-        public bool SendAnswer(ChallengeClient challengeClient, ChallengeResponse challenge, string type)
+        public bool SendAnswer(ChallengeClient challengeClient, ChallengeResponse challenge)
         {
             string round = challenge.Rounds[challenge.Rounds.Count - 1].Id;
             //Console.WriteLine($"Нажми ВВОД, чтобы получить задачу типа {type}");
             //Console.ReadLine();
-            var newTask = challengeClient.AskNewTaskAsync(round, type).Result;
+            var newTask = challengeClient.AskNewTaskAsync(round).Result;
             Console.WriteLine($"  Новое задание, статус {newTask.Status}");
             Console.WriteLine($"  Формулировка: {newTask.UserHint}");
             Console.WriteLine($"                {newTask.Question}");
 
-            string answer = GetAns(type, newTask.Question);
+            string answer = GetAns(newTask.UserHint, newTask.Question);
             //Console.WriteLine($"Нажми ВВОД, чтобы ответить на полученную задачу самым правильным ответом: {answer}");
             //Console.ReadLine();
             var updatedTask = challengeClient.CheckTaskAnswerAsync(newTask.Id, answer).Result;
