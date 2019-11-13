7ec567454d69a5528a5b18824c44e666ed7d38a4
trololozhkin <alexand3r.lozhkin@gmail.com>
2019-11-14 01:44:27 +0500

    a?

--- challenge-starterkit-master/ConsoleCoreApp/Program.cs
+++ challenge-starterkit-master/ConsoleCoreApp/Program.cs
@@ -13,14 +13,14 @@ namespace ConsoleCoreApp
             const string challengeId = "projects-course";
             var challenge = challengeClient.GetChallengeAsync(challengeId).Result;
             var sender = new Sender();
-            sender.ShowResults(challengeClient);
-            Console.WriteLine("Insert type of your task");
-            string type = Console.ReadLine();
+            //sender.ShowResults(challengeClient);
+            //Console.WriteLine("Insert type of your task");
+            //string type = Console.ReadLine();
             Console.WriteLine("Insert number of tests");
             var count = int.Parse(Console.ReadLine());    
-            sender.SendSomeAnswers(challengeClient, challenge, type, count);
+            sender.SendSomeAnswers(challengeClient, challenge, count);
             //*/
-            Console.WriteLine(Caesar.GetAns("first longest word=quirrell|7c5zmcp24z7yz7qvyz yywzecze9phd4z7yo4zeyywzspd''y22z97y'yz97q9zvy'3z4q3ze7qkywz7qw4ez5d97z7dlzdwz97y"));
+            Console.WriteLine(InverseMatrix.GetAnswer(@"3 & -6 \\ -6 & 8"));
         }
     }
 }
