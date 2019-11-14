11ea0ca9cd0dcb5d0424c00fb1b3da8ebe1be93d
Nikita <42915946+asdminerpro@users.noreply.github.com>
2019-11-14 15:06:14 +0500

    Updoot ComplexSolver.py
    itwassoeasy,lozhkin,prinimaj
    fixed output but check pls

--- challenge-starterkit-master/ConsoleCoreApp/ComplexSolver.py
+++ challenge-starterkit-master/ConsoleCoreApp/ComplexSolver.py
@@ -1,30 +1,30 @@
 s = input()
 
-s = s.replace("i","*i")
-s = s.replace("-*","-")
-s = s.replace("+*","+")
-s = s.replace("**","*")
-s = s.replace("(*","(")
+s = s.replace("i", "*i")
+s = s.replace("-*", "-")
+s = s.replace("+*", "+")
+s = s.replace("**", "*")
+s = s.replace("(*", "(")
 if (s[0] == '*'):
     s = s[1:]
 
-s = s.replace("(","*(")
-s = s.replace("-*","-")
-s = s.replace("+*","+")
-s = s.replace("**","*")
-s = s.replace("(*","(")
+s = s.replace("(", "*(")
+s = s.replace("-*", "-")
+s = s.replace("+*", "+")
+s = s.replace("**", "*")
+s = s.replace("(*", "(")
 if (s[0] == '*'):
     s = s[1:]
 
-s = s.replace(")",")*")
-s = s.replace("*-","-")
-s = s.replace("*+","+")
-s = s.replace("**","*")
+s = s.replace(")", ")*")
+s = s.replace("*-", "-")
+s = s.replace("*+", "+")
+s = s.replace("**", "*")
 if (s[-1] == '*'):
     s = s[:-1]
 
 s = "(" + s + ")"
-s = s.replace("i","j")
+s = s.replace("i", "j")
 
 # print(s)
 
@@ -35,14 +35,14 @@ enddig = 0
 i = 1
 while i < len(s):
     if s[i] == 'j':
-        s = str(s[:i]) + "(0+1j)" + str(s[i+1:])
+        s = str(s[:i]) + "(0+1j)" + str(s[i + 1:])
         i += 5
     else:
         if s[i] in digs and s[i - 1] not in digs:
             startdig = i
         if s[i] not in digs and s[i - 1] in digs:
             enddig = i - 1
-    
+
         if enddig > 0:
             s = str(s[:startdig]) + "(" + str(s[startdig:enddig + 1]) + "+0j)" + str(s[enddig + 1:])
             i = enddig + 6
@@ -52,11 +52,27 @@ while i < len(s):
     i += 1
 
 # print(s)
-        
+
 # print(eval(s))
-ans = str(eval(s))
-ans = ans.replace("j","i")
-ans = ans.replace("(","")
-ans = ans.replace(")","")
+num = eval(s)
+real = num.real
+imag = num.imag
+# ans = str(eval(s))
+# ans = ans.replace("j", "i")
+# ans = ans.replace("(", "")
+# ans = ans.replace(")", "")
+# print(ans)
+
+ans = ""
+if (abs(real) < 1e-9 and abs(imag) < 1e-9):
+    ans = "0"
+elif (abs(real) < 1e-9):
+    ans = str(int(imag)) + "i"
+elif (abs(imag) < 1e-9):
+    ans = str(int(real))
+elif (imag > 1e-9):
+    ans = str(int(real)) + "+" + str(int(imag)) + "i"
+else:
+    ans = str(int(real)) + str(int(imag)) + "i"
 
 print(ans)
