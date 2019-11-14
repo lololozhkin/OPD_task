81e1cc5974f440e60846e7698bdbe21d4770d799
Stepan Yurchenko <stepan33314@yandex.ru>
2019-11-14 17:11:26 +0500

    Commoot in.txt out.txt timer.py Updoot ComplexSolver.py Inserting dick in lozhkin

--- challenge-starterkit-master/ConsoleCoreApp/ComplexSolver.py
+++ challenge-starterkit-master/ConsoleCoreApp/ComplexSolver.py
@@ -1,100 +1,99 @@
-s = input()
+def complex_solver(s):
+    s = s.replace(" ", "")
 
-s = s.replace(" ", "")
+    s = s.replace("i", "*i")
+    s = s.replace("-*", "-")
+    s = s.replace("+*", "+")
+    s = s.replace("**", "*")
+    s = s.replace("(*", "(")
+    if (s[0] == '*'):
+        s = s[1:]
 
-s = s.replace("i", "*i")
-s = s.replace("-*", "-")
-s = s.replace("+*", "+")
-s = s.replace("**", "*")
-s = s.replace("(*", "(")
-if (s[0] == '*'):
-    s = s[1:]
+    s = s.replace("(", "*(")
+    s = s.replace("-*", "-")
+    s = s.replace("+*", "+")
+    s = s.replace("**", "*")
+    s = s.replace("(*", "(")
+    if (s[0] == '*'):
+        s = s[1:]
 
-s = s.replace("(", "*(")
-s = s.replace("-*", "-")
-s = s.replace("+*", "+")
-s = s.replace("**", "*")
-s = s.replace("(*", "(")
-if (s[0] == '*'):
-    s = s[1:]
+    s = s.replace("i", "i*")
+    s = s.replace("*-", "-")
+    s = s.replace("*+", "+")
+    s = s.replace("**", "*")
+    s = s.replace("*)", ")")
+    if (s[-1] == '*'):
+        s = s[:-1]
 
-s = s.replace("i", "i*")
-s = s.replace("*-", "-")
-s = s.replace("*+", "+")
-s = s.replace("**", "*")
-s = s.replace("*)", ")")
-if (s[-1] == '*'):
-    s = s[:-1]
+    s = s.replace(")", ")*")
+    s = s.replace("*-", "-")
+    s = s.replace("*+", "+")
+    s = s.replace("**", "*")
+    s = s.replace("*)", ")")
+    if (s[-1] == '*'):
+        s = s[:-1]
 
-s = s.replace(")", ")*")
-s = s.replace("*-", "-")
-s = s.replace("*+", "+")
-s = s.replace("**", "*")
-s = s.replace("*)", ")")
-if (s[-1] == '*'):
-    s = s[:-1]
+    s = "(" + s + ")"
+    s = s.replace("i", "j")
+    
+    # print(s)
 
-s = "(" + s + ")"
-s = s.replace("i", "j")
+    digs = "1234567890"
 
-# print(s)
+    startdig = 0
+    enddig = 0
+    i = 1
+    while i < len(s):
+        if s[i] == 'j':
+            s = str(s[:i]) + "(0+1j)" + str(s[i + 1:])
+            i += 5
+        else:
+            if s[i] in digs and s[i - 1] not in digs:
+                startdig = i
+            if s[i] not in digs and s[i - 1] in digs:
+                enddig = i - 1
 
-digs = "1234567890"
+            if enddig > 0:
+                s = str(s[:startdig]) + "(" + str(s[startdig:enddig + 1]) + "+0j)" + str(s[enddig + 1:])
+                i = enddig + 6
+                startdig = 0
+                enddig = 0
 
-startdig = 0
-enddig = 0
-i = 1
-while i < len(s):
-    if s[i] == 'j':
-        s = str(s[:i]) + "(0+1j)" + str(s[i + 1:])
-        i += 5
+        i += 1
+
+    # print(s)
+
+    # print(eval(s))
+    num = eval(s)
+    real = num.real
+    imag = num.imag
+    # ans = str(eval(s))
+    # ans = ans.replace("j", "i")
+    # ans = ans.replace("(", "")
+    # ans = ans.replace(")", "")
+    # print(ans)
+    
+    ans = ""
+    if (abs(real) < 1e-9 and abs(imag) < 1e-9):
+        ans = "0"
+    elif (abs(real) < 1e-9):
+        if (abs(imag - 1) < 1e-9):
+            ans = "i"
+        elif (abs(imag + 1) < 1e-9):
+            ans = "-i"
+        else:
+            ans = str(int(imag)) + "i"
+    elif (abs(imag) < 1e-9):
+        ans = str(int(real))
+    elif (imag > 1e-9):
+        if (abs(imag - 1) < 1e-9):
+            ans = str(int(real)) + "+i"
+        else:
+            ans = str(int(real)) + "+" + str(int(imag)) + "i"
     else:
-        if s[i] in digs and s[i - 1] not in digs:
-            startdig = i
-        if s[i] not in digs and s[i - 1] in digs:
-            enddig = i - 1
+        if (abs(imag + 1) < 1e-9):
+            ans = str(int(real)) + "-i"
+        else:
+            ans = str(int(real)) + str(int(imag)) + "i"
 
-        if enddig > 0:
-            s = str(s[:startdig]) + "(" + str(s[startdig:enddig + 1]) + "+0j)" + str(s[enddig + 1:])
-            i = enddig + 6
-            startdig = 0
-            enddig = 0
-
-    i += 1
-
-# print(s)
-
-# print(eval(s))
-num = eval(s)
-real = num.real
-imag = num.imag
-# ans = str(eval(s))
-# ans = ans.replace("j", "i")
-# ans = ans.replace("(", "")
-# ans = ans.replace(")", "")
-# print(ans)
-
-ans = ""
-if (abs(real) < 1e-9 and abs(imag) < 1e-9):
-    ans = "0"
-elif (abs(real) < 1e-9):
-    if (abs(imag - 1) < 1e-9):
-        ans = "i"
-    elif (abs(imag + 1) < 1e-9):
-        ans = "-i"
-    else:
-        ans = str(int(imag)) + "i"
-elif (abs(imag) < 1e-9):
-    ans = str(int(real))
-elif (imag > 1e-9):
-    if (abs(imag - 1) < 1e-9):
-        ans = str(int(real)) + "+i"
-    else:
-        ans = str(int(real)) + "+" + str(int(imag)) + "i"
-else:
-    if (abs(imag + 1) < 1e-9):
-        ans = str(int(real)) + "-i"
-    else:
-        ans = str(int(real)) + str(int(imag)) + "i"
-
-print(ans)
+    return ans
---
+++ challenge-starterkit-master/ConsoleCoreApp/timer.py
@@ -0,0 +1,15 @@
+import time
+from ComplexSolver import complex_solver
+
+if __name__ == '__main__':
+    while True:
+        time.sleep(10)
+        answer = ''
+        with open('in.txt', 'r', encoding='utf-8') as data_file:
+            data = data_file.readlines()
+            if data:
+                answer = complex_solver(data[0][:-1])
+        with open('in.txt', 'w', encoding='utf-8') as f:
+            pass
+        with open('out.txt', 'w', encoding='utf-8') as ans_file:
+            ans_file.write(answer)
